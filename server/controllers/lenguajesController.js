2f4adc33de4b751bb9306d53e7bbef5dc66b2d72
Stepalom77 <stepalom22@gmail.com>
2022-12-10 13:48:42 -0500

    metodo y ruta put agregada

--- server/controllers/lenguajesController.js
+++ server/controllers/lenguajesController.js
@@ -46,10 +46,31 @@ const lenguaje = require('../models/lenguajes')
         return res.status(201).json(savedLenguaje);
     };
 
+    const updateLenguaje =  async (req, res) => {
+        let lenguajeSearched = null;
+        let lenguajeId = req.params.id;
+        let lenguajeUpdated = null;
+        try {
+           lenguajeSearched =  await lenguaje.findById(lenguajeId);
+           lenguajeUpdated =  await lenguaje.findByIdAndUpdate(lenguajeId, req.body, {
+               new: true,
+           })
+        } catch (err) {
+            console.error(err);
+            if (!lenguajeSearched) {
+                return res.status(404).json({message: 'Error, el lenguaje que estas tratando de modificar no existe'})
+            } else {
+                return res.status(400).json({message: 'Huvo un error'})
+            };
+        };
+        return res.status(200).json(lenguajeUpdated);
+    };
+
      module.exports = {
         getAll: getLenguajes,
         getOne: getLenguaje,
         create: createLenguaje,
+        update: updateLenguaje,
      }
 
  
--- server/routes/lenguajesRoutes.js
+++ server/routes/lenguajesRoutes.js
@@ -5,5 +5,6 @@ const lenguaje = require("../controllers/lenguajesController");
 router.get('/lenguaje', lenguaje.getAll);
 router.post('/lenguaje', lenguaje.create);
 router.get('/lenguaje/:id', lenguaje.getOne);
+router.put('/lenguaje/:id', lenguaje.update);
 
 module.exports = router;
