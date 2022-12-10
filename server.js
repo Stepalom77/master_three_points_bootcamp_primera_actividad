e9e0c22bc4b7b9888c32c8dbf7dfe26627fd042a
Stepalom77 <stepalom22@gmail.com>
2022-12-10 12:18:50 -0500

    mongodb connection completado

--- server.js
+++ server.js
@@ -2,15 +2,31 @@ require('dotenv').config()
 const express = require('express');
 const morgan = require('morgan');
 const cors = require('cors');
+const mongoose = require('mongoose');
 
 const app = express();
 const PORT = process.env.PORT || 2000;
+const mongoUrl = process.env.ME_CONFIG_MONGODB_URL || 'mongodb://root:example@mongodb'
 
 //Middlewares 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(morgan('dev'));
 app.use(cors());
+
+mongoose.connect(
+  mongoUrl, 
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+  }
+);
+
+const db = mongoose.connection;
+db.on("error", console.error.bind(console, "connection error: "));
+db.once("open", function () {
+  console.log("Connected successfully");
+});
 
 app.get('/', (req, res) => {
     res.send('Servidor en linea ✅');
